8ea824bb280539e6d8d44143c89cc003113bd753
prahlad pawar <prahladpawar@prahlads-MacBook-Air.local>
2022-09-09 13:58:47 +0530

    version 2.0

--- script/index.js
+++ script/index.js
@@ -1,6 +1,9 @@
 let va = document.getElementById("counter")
 let saveEl = document.getElementById("saved-el");
+let va2 = document.getElementById("counter-2")
+let saveEl2 = document.getElementById("saved-el-2");
 let count = 0;
+let count2 = 0;
 function Increament() {
     count++;
     va.innerText = count;
@@ -14,6 +17,21 @@ function save() {
     va.textContent = 0;
     if (saveEl.textContent.length > 35) {
         saveEl.textContent = "";
+    }
+}
+function Increament2() {
+    count2++;
+    va2.innerText = count2;
+}
+function save2() {
+    if (count2 == 0) {
+        return;
+    }
+    saveEl2.textContent += count2 + " - ";
+    count2 = 0;
+    va2.textContent = 0;
+    if (saveEl2.textContent.length > 35) {
+        saveEl2.textContent = "";
     }
 }
 
